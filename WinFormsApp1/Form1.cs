c77886507601d7d2f591f4df1298d9cf323ddc62
Mohammed Amine Mostefai <mostefaiamine@gmail.com>
2021-10-10 18:21:28 +0200

    first commit

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WK.Libraries.SharpClipboardNS;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var clipboard = new SharpClipboard();
+            clipboard.ClipboardChanged += Clipboard_ClipboardChanged;
+        }
+
+        private void Clipboard_ClipboardChanged(object sender, SharpClipboard.ClipboardChangedEventArgs e)
+        {
+           if(e.ContentType == SharpClipboard.ContentTypes.Text)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Paste();
+        }
+
+    }
+}
